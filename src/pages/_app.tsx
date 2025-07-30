1aaa50f66d125484b212d7b41c9f866313567c77
ruan <ruan.2023014206@alunocesul.com>
2025-07-29 21:52:12 -0300

    chore: theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from "next/app";
 import { Provider } from "@/components/ui/provider";
 
+
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
