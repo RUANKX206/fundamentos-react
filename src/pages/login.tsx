6a15f36a39899da56e126f77fad5d7d15442b00f
ruan <ruan.2023014206@alunocesul.com>
2025-07-29 22:40:47 -0300

    create: imagem and divs

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,20 @@
+import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import loginImage from "../../public/assets/login-image.gif"
+
 export default function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <Flex w="100vw" h="100vh">
+      <Flex w="50%" bg= "#2C73EB" align="center" justify="center">
+        <Image w={500} h={500}src={loginImage.src}/>
+      </Flex>
+
+      <VStack w="50%" bg="gray.300" align="start">
+
+        <Heading as={"h1"}> Account Login</Heading>
+        <Text> you are already a member you can login with your email address and password</Text>
+
+
+      </VStack>
+    </Flex>
   )
 }
